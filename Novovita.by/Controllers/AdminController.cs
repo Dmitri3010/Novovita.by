6cb1cdaac97ee53974d9cf8f5d4ad67b4c089c36
Dmitri <lipov758@gmail.com>
2019-03-25 00:10:14 +0300

    Add admin controller

---
+++ Novovita.by/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novovita.by.Controllers
+{
+    public class AdminController:Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Admin";
+
+            return View();
+        }
+
+        public IActionResult CategoriesList()
+        {
+            return View();
+        }
+    }
+}
