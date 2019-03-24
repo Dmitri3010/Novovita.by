06bd1889ac7d920139243c1cb78fed8c0f3aec97
Dmitri <lipov758@gmail.com>
2019-03-25 00:09:27 +0300

    Add base models

---
+++ Novovita.by/Models/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novovita.by.Models
+{
+    public class Base
+    {
+        public int id { get; set; }
+    }
+}
---
+++ Novovita.by/Models/Contacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novovita.by.Models
+{
+    public class Contacts:Base
+    {
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string  Adress { get; set; }
+
+
+    }
+}
