a6ee309d58377eb04a933a41dd04fbf81b3820b5
Dmitri <lipov758@gmail.com>
2019-03-26 01:20:06 +0300

    Add repository class for categories

---
+++ Novovita.by/Services/Repository/Categories.cs
@@ -0,0 +1,23 @@
+﻿using Novovita.by.Context;
+using Novovita.by.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Novovita.by.Services.Repository
+{
+
+    public class Categories
+    {
+        static EfContext db = new EfContext();
+
+        public static List<Category> Get()
+        {
+            return db.Categories.ToList();
+        }
+
+        public static Category Get(int id)
+        {
+            return db.Categories.FirstOrDefault(p => p.id == id);
+        }
+    }
+}
