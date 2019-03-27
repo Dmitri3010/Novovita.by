fa32ff297fd54e044326eae4d20fc6b3b8ad32e0
Dmitri <lipov758@gmail.com>
2019-03-27 14:44:08 +0300

    Add new repository classes

---
+++ Novovita.by/Repository/News.cs
@@ -0,0 +1,16 @@
+﻿using Novovita.by.Context;
+using Novovita.by.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novovita.by.Repository
+{
+    public class NewsRepository
+    {
+        static EfContext db = new EfContext();
+
+        public static List<News> Get => db.News.ToList();
+    }
+}
