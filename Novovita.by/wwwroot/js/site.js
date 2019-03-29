78713f460e4bc0ac7ee2bd94c20e49cd96dff4ef
Dmitri <lipov758@gmail.com>
2019-03-30 01:50:18 +0300

    Add particial view for products page

---
+++ Novovita.by/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿function getProducts(idForUpdate, categoryId) {
+    $.ajax({
+        type: "GET",
+        url: "/Home/Filtered?categoryId=" + categoryId,
+        success: function (response) {
+            console.log('get products response', response);
+            $(idForUpdate).html(response);
+        },
+        error: function (response) {
+            $(idForUpdate).html(response.responseText);
+            console.error(response);
+        }
+    });
+}
