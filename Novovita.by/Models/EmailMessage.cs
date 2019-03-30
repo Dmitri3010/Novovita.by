0c480bd0bff09d7cd25b370a27f689e67a42ca1c
Dmitri <lipov758@gmail.com>
2019-03-30 23:04:24 +0300

    Add email message model

---
+++ Novovita.by/Models/EmailMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Novovita.by.Models
+{
+    public class EmailMessage:Base
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
