e83a43561f56908bac4a1ee09117379b34af7221
Dmitri <lipov758@gmail.com>
2019-03-30 23:03:03 +0300

    Fix UI bugs

--- Novovita.by/wwwroot/js/site.js
+++ Novovita.by/wwwroot/js/site.js
@@ -3,7 +3,6 @@
         type: "GET",
         url: "/Home/Filtered?categoryId=" + categoryId,
         success: function (response) {
-            console.log('get products response', response);
             $(idForUpdate).html(response);
         },
         error: function (response) {
